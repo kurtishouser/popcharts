0e78056a665732fbcd9d2f5476adbe10eefdea95
Kurtis Houser <houserkurtis@gmail.com>
2017-05-16 16:07:39 -0700

    Add form fuctionality to update charts

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
-getPopulationByYearAndCountry(2010, 'United States')
-  .then(populationData => drawLineChart('#chart', populationData, 0))
-  .catch(catchErr);
+// uncomment these to start with populated graphs
+// getPopulationByYearAndCountry(2010, 'United States')
+//   .then(populationData => drawLineChart('#year-chart', populationData, 0))
+//   .catch(catchErr);
+//
+// getPopulationByAgeGroupAndCountry(25, 'United States')
+//   .then(populationData => drawLineChart('#age-chart', populationData, 1950))
+//   .catch(catchErr);
 
-getPopulationByAgeGroupAndCountry(25, 'United States')
-  .then(populationData => drawLineChart('#chart2', populationData, 1950))
-  .catch(catchErr);
 
-getTotlaLifeExpectancy('male', 'United States', '1980-01-01')
-  .then(logAndContinue)
-  .catch(catchErr);
+// getTotlaLifeExpectancy('male', 'United States', '1980-01-01')
+//   .then(logAndContinue)
+//   .catch(catchErr);
 
 // Retrieve population tables for a given year and country
 function getPopulationByYearAndCountry(year, country) {
@@ -111,3 +113,24 @@ function drawLineChart(domId, columnValues, xAxisStartTick) {
     }
   });
 }
+
+/* ----- FORM SUBMISSION - move to separate file ----- */
+$("#age-chart-form").on("submit", function() {
+  let year = $('#age-chart-year').val();
+  let country = $('#age-chart-country').val();
+  getPopulationByYearAndCountry(year, country)
+    .then(populationData => drawLineChart('#age-chart', populationData, 0))
+    .catch(catchErr);
+  return false;
+});
+
+$("#year-chart-form").on("submit", function() {
+  console.log('submitted');
+  let age = $('#year-chart-age').val();
+  let country = $('#year-chart-country').val();
+  getPopulationByAgeGroupAndCountry(age, country)
+    .then(logAndContinue)
+    .then(populationData => drawLineChart('#year-chart', populationData, 1950))
+    .catch(catchErr);
+  return false;
+});
