3ffbae7f1ef035445d0fca5b8968ba41d84a9ddb
Kurtis Houser <houserkurtis@gmail.com>
2017-05-17 22:52:55 -0700

    Update country and life expectancy charts

--- js/app.js
+++ js/app.js
@@ -1,4 +1,243 @@
-// uncomment these to start with populated graphs
+const countries = [
+  "Afghanistan",
+  // "AFRICA",
+  "Albania",
+  "Algeria",
+  "Angola",
+  "Antigua and Barbuda",
+  "Arab Rep of Egypt",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  // "ASIA",
+  "Australia",
+  // "Australia/New Zealand",
+  "Austria",
+  "Azerbaijan",
+  // "The Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei Darussalam",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cote-d-Ivoire",
+  "Cabo Verde",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  // "Caribbean",
+  "Central African Republic",
+  // "Central America",
+  // "Central Asia",
+  "Chad",
+  "Channel Islands",
+  "Chile",
+  "China",
+  "Hong Kong SAR-China",
+  "Macao SAR China",
+  "Colombia",
+  "Comoros",
+  "Congo",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Curacao",
+  "Cyprus",
+  "Czech Republic",
+  "Dem Peoples Rep of Korea",
+  "Dem Rep of Congo",
+  "Denmark",
+  "Djibouti",
+  "Dominican Republic",
+  // "Eastern Africa",
+  // "Eastern Asia",
+  // "Eastern Europe",
+  "Ecuador",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Ethiopia",
+  // "EUROPE",
+  "Federated States of Micronesia",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Guiana",
+  "French Polynesia",
+  "FYR Macedonia",
+  "Gabon",
+  "The Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guadeloupe",
+  "Guam",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Islamic Republic of Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kuwait",
+  "Kyrgyz Republic",
+  "Lao PDR",
+  // "LATIN AMERICA AND THE CARIBBEAN",
+  "Latvia",
+  // "Least developed countries",
+  "Lebanon",
+  "Lesotho",
+  // "Less developed regions",
+  // "Less developed regions, excluding China",
+  // "Less developed regions, excluding least developed countries",
+  "Liberia",
+  "Libya",
+  "Lithuania",
+  "Luxembourg",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Martinique",
+  "Mauritania",
+  "Mauritius",
+  "Mayotte",
+  // "Melanesia",
+  "Mexico",
+  // "Micronesia",
+  // "Middle Africa",
+  "Moldova",
+  "Mongolia",
+  "Montenegro",
+  // "More developed regions",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nepal",
+  "The Netherlands",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  // "Northern Africa",
+  // "NORTHERN AMERICA",
+  // "Northern Europe",
+  "Norway",
+  // "OCEANIA",
+  "Oman",
+  // "Other non-specified areas",
+  "Pakistan",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  // "Polynesia",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Reunion",
+  "RB-de-Venezuela",
+  "Rep of Korea",
+  "Rep of Yemen",
+  "Romania",
+  "Russian Federation",
+  "Rwanda",
+  "St-Lucia",
+  "St-Vincent and the Grenadines",
+  "Samoa",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovak Republic",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  // "South America",
+  "South Sudan",
+  // "South-Central Asia",
+  // "South-Eastern Asia",
+  // "Southern Africa",
+  // "Southern Asia",
+  // "Southern Europe",
+  "Spain",
+  "Sri Lanka",
+  "West Bank and Gaza",
+  // "Sub-Saharan Africa",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syrian Arab Rep",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "US Virgin Islands",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Vietnam",
+  // "Western Africa",
+  // "Western Asia",
+  // "Western Europe",
+  // "Western Sahara",
+  // "World",
+  "Zambia",
+  "Zimbabwe"
+];
+
+/* --- uncomment these to start with populated graphs --- */
 // getPopulationByYearAndCountry(2010, 'United States')
 //   .then(populationData => drawLineChart('#year-chart', populationData, 0))
 //   .catch(catchErr);
@@ -6,16 +245,14 @@
 // getPopulationByAgeGroupAndCountry(25, 'United States')
 //   .then(populationData => drawLineChart('#age-chart', populationData, 1950))
 //   .catch(catchErr);
-
+//
 // getPopulationByAgeGroupAndYear(30, 2015)
-//   .then(logAndContinue)
-//   .then(populationData => drawBarChart('#country-chart', populationData, 0))
+//   .then(populationData => drawCountryBarChart('#country-chart', populationData))
 //   .catch(catchErr);
+//
+// getTotalLifeExpectancyAllCountries('male', 'United States', '2000-01-01')
+//   .then(totalLifeExpectancy => drawLifeExpectancyBarChart('#life-expectancy-chart', totalLifeExpectancy))
 
-
-// getTotalLifeExpectancy('male', 'United States', '1980-01-01')
-//   .then(logAndContinue)
-//   .catch(catchErr);
 
 // Retrieve population tables for a given year and country
 function getPopulationByYearAndCountry(year, country) {
@@ -69,268 +306,27 @@ function getPopulationByAgeGroupAndCountry(age, country) {
 function getPopulationByAgeGroupAndYear(age, year) {
   let url = `http://api.population.io/1.0/population/${year}/aged/${age}/`;
   return getJsonFromFetch(url)
-    .then(populationTable => {
-        let columnValues = [];
-        let males = ['M - ' +  age + ' yrs - ' + year];
-        let females = ['F - ' +  age + ' yrs - ' + year];
-        let total = ['Total - ' +  age + ' yrs - ' + year];
-
-        populationTable.forEach(item => {
-            males.push(item.males);
-            females.push(item.females);
-            total.push(item.total);
-          });
-
-        columnValues.push(males);
-        columnValues.push(females);
-        columnValues.push(total);
-        return columnValues;
-    })
+    // .then(populationTable => {
+    //     let columnValues = [];
+    //     let males = ['M - ' +  age + ' yrs - ' + year];
+    //     let females = ['F - ' +  age + ' yrs - ' + year];
+    //     let total = ['Total - ' +  age + ' yrs - ' + year];
+    //
+    //     populationTable.forEach(item => {
+    //         males.push(item.males);
+    //         females.push(item.females);
+    //         total.push(item.total);
+    //       });
+    //
+    //     columnValues.push(males);
+    //     columnValues.push(females);
+    //     columnValues.push(total);
+    //     return columnValues;
+    // })
 }
 
 // Calculate total life expectancy - all countries
 function getTotalLifeExpectancyAllCountries(sex, country, dob) {
-  // var sex = 'male';
-  // var dob = '1990-01-01'
-  const countries = [
-    "Afghanistan",
-    // "AFRICA",
-    "Albania",
-    "Algeria",
-    "Angola",
-    "Antigua and Barbuda",
-    "Arab Rep of Egypt",
-    "Argentina",
-    "Armenia",
-    "Aruba",
-    // "ASIA",
-    "Australia",
-    // "Australia/New Zealand",
-    "Austria",
-    "Azerbaijan",
-    // "The Bahamas",
-    "Bahrain",
-    "Bangladesh",
-    "Barbados",
-    "Belarus",
-    "Belgium",
-    "Belize",
-    "Benin",
-    "Bhutan",
-    "Bolivia",
-    "Bosnia and Herzegovina",
-    "Botswana",
-    "Brazil",
-    "Brunei Darussalam",
-    "Bulgaria",
-    "Burkina Faso",
-    "Burundi",
-    "Cote-d-Ivoire",
-    "Cabo Verde",
-    "Cambodia",
-    "Cameroon",
-    "Canada",
-    // "Caribbean",
-    "Central African Republic",
-    // "Central America",
-    // "Central Asia",
-    "Chad",
-    "Channel Islands",
-    "Chile",
-    "China",
-    "Hong Kong SAR-China",
-    "Macao SAR China",
-    "Colombia",
-    "Comoros",
-    "Congo",
-    "Costa Rica",
-    "Croatia",
-    "Cuba",
-    "Curacao",
-    "Cyprus",
-    "Czech Republic",
-    "Dem Peoples Rep of Korea",
-    "Dem Rep of Congo",
-    "Denmark",
-    "Djibouti",
-    "Dominican Republic",
-    // "Eastern Africa",
-    // "Eastern Asia",
-    // "Eastern Europe",
-    "Ecuador",
-    "El Salvador",
-    "Equatorial Guinea",
-    "Eritrea",
-    "Estonia",
-    "Ethiopia",
-    // "EUROPE",
-    "Federated States of Micronesia",
-    "Fiji",
-    "Finland",
-    "France",
-    "French Guiana",
-    "French Polynesia",
-    "FYR Macedonia",
-    "Gabon",
-    "The Gambia",
-    "Georgia",
-    "Germany",
-    "Ghana",
-    "Greece",
-    "Grenada",
-    "Guadeloupe",
-    "Guam",
-    "Guatemala",
-    "Guinea",
-    "Guinea-Bissau",
-    "Guyana",
-    "Haiti",
-    "Honduras",
-    "Hungary",
-    "Iceland",
-    "India",
-    "Indonesia",
-    "Islamic Republic of Iran",
-    "Iraq",
-    "Ireland",
-    "Israel",
-    "Italy",
-    "Jamaica",
-    "Japan",
-    "Jordan",
-    "Kazakhstan",
-    "Kenya",
-    "Kiribati",
-    "Kuwait",
-    "Kyrgyz Republic",
-    "Lao PDR",
-    // "LATIN AMERICA AND THE CARIBBEAN",
-    "Latvia",
-    // "Least developed countries",
-    "Lebanon",
-    "Lesotho",
-    // "Less developed regions",
-    // "Less developed regions, excluding China",
-    // "Less developed regions, excluding least developed countries",
-    "Liberia",
-    "Libya",
-    "Lithuania",
-    "Luxembourg",
-    "Madagascar",
-    "Malawi",
-    "Malaysia",
-    "Maldives",
-    "Mali",
-    "Malta",
-    "Martinique",
-    "Mauritania",
-    "Mauritius",
-    "Mayotte",
-    // "Melanesia",
-    "Mexico",
-    // "Micronesia",
-    // "Middle Africa",
-    "Moldova",
-    "Mongolia",
-    "Montenegro",
-    // "More developed regions",
-    "Morocco",
-    "Mozambique",
-    "Myanmar",
-    "Namibia",
-    "Nepal",
-    "The Netherlands",
-    "New Caledonia",
-    "New Zealand",
-    "Nicaragua",
-    "Niger",
-    "Nigeria",
-    // "Northern Africa",
-    // "NORTHERN AMERICA",
-    // "Northern Europe",
-    "Norway",
-    // "OCEANIA",
-    "Oman",
-    // "Other non-specified areas",
-    "Pakistan",
-    "Panama",
-    "Papua New Guinea",
-    "Paraguay",
-    "Peru",
-    "Philippines",
-    "Poland",
-    // "Polynesia",
-    "Portugal",
-    "Puerto Rico",
-    "Qatar",
-    "Reunion",
-    "RB-de-Venezuela",
-    "Rep of Korea",
-    "Rep of Yemen",
-    "Romania",
-    "Russian Federation",
-    "Rwanda",
-    "St-Lucia",
-    "St-Vincent and the Grenadines",
-    "Samoa",
-    "Sao Tome and Principe",
-    "Saudi Arabia",
-    "Senegal",
-    "Serbia",
-    "Seychelles",
-    "Sierra Leone",
-    "Singapore",
-    "Slovak Republic",
-    "Slovenia",
-    "Solomon Islands",
-    "Somalia",
-    "South Africa",
-    // "South America",
-    "South Sudan",
-    // "South-Central Asia",
-    // "South-Eastern Asia",
-    // "Southern Africa",
-    // "Southern Asia",
-    // "Southern Europe",
-    "Spain",
-    "Sri Lanka",
-    "West Bank and Gaza",
-    // "Sub-Saharan Africa",
-    "Sudan",
-    "Suriname",
-    "Swaziland",
-    "Sweden",
-    "Switzerland",
-    "Syrian Arab Rep",
-    "Tajikistan",
-    "Tanzania",
-    "Thailand",
-    "Timor-Leste",
-    "Togo",
-    "Tonga",
-    "Trinidad and Tobago",
-    "Tunisia",
-    "Turkey",
-    "Turkmenistan",
-    "Uganda",
-    "Ukraine",
-    "United Arab Emirates",
-    "United Kingdom",
-    "United States",
-    "US Virgin Islands",
-    "Uruguay",
-    "Uzbekistan",
-    "Vanuatu",
-    "Vietnam",
-    // "Western Africa",
-    // "Western Asia",
-    // "Western Europe",
-    // "Western Sahara",
-    // "World",
-    "Zambia",
-    "Zimbabwe"
-  ];
-
   let url = `http://api.population.io/1.0/life-expectancy/total/`;
   let lifeExpectancyRequests = countries.map(country => fetch(`${url}${sex}/${country}/${dob}/`))
   return Promise.all(lifeExpectancyRequests)
@@ -338,27 +334,34 @@ function getTotalLifeExpectancyAllCountries(sex, country, dob) {
     let lifeExpectancyPromises = lifeExpectancyResponses.map(lifeExpectancyResponse => lifeExpectancyResponse.json()); // array of promises
     return Promise.all(lifeExpectancyPromises)
   })
-  .then(lifeExpectancies => {
-    let totalLifeExpectancy = lifeExpectancies.map(lifeExpectancy => lifeExpectancy.total_life_expectancy);
-    totalLifeExpectancy.unshift('Life Expectancy')
-    return totalLifeExpectancy;
+  .then(data => {
+    return data.sort(function(a, b){
+      return a.total_life_expectancy - b.total_life_expectancy;
+    })
   })
+  // .then(logAndContinue)
+  // .then(lifeExpectancies => {
+  //   let totalLifeExpectancy = lifeExpectancies.map(lifeExpectancy => {
+  //     // life.country = lifeExpectancy.country;
+  //     // life.lifeExpectancy.total_life_expectancy;
+  //     return lifeExpectancy.total_life_expectancy;
+  //   })
+  //   totalLifeExpectancy.unshift('Life Expectancy')
+  //   return totalLifeExpectancy;
+  // })
   // .then(logAndContinue)
 
 }
-getTotalLifeExpectancyAllCountries('male', 'United States', '2000-01-01')
-  .then(logAndContinue).
-  then(totalLifeExpectancy => drawLifeExpectancyBarChart('#life-expectancy-chart', [totalLifeExpectancy.sort()], 0))
 
-// Calculate total life expectancy - individual country
-function getTotalLifeExpectancy(sex, country, dob) {
+// NOT USED - Calculate total life expectancy - individual country
+function getTotalLifeExpectancyIndividual(sex, country, dob) {
   let url = `http://api.population.io/1.0/life-expectancy/total/${sex}/${country}/${dob}/`
   return getJsonFromFetch(url)
     .then(json => json.total_life_expectancy);
 }
 
 function getJsonFromFetch(url) {
-  console.log('Requesting', url);
+  // console.log('Requesting', url);
   return fetch(url)
     .then(response => response.json());
 }
@@ -372,9 +375,9 @@ function logAndContinue(value) {
   return value;
 }
 
+
 /* ----- PRESENTATIONAL - move to separate file ----- */
 function drawLineChart(domId, chartData, xAxisStartTick, xAxisLabel) {
-  console.log('chartData\n', chartData);
   var chart = c3.generate({
     bindto: domId,
     data: {
@@ -410,92 +413,121 @@ function drawLineChart(domId, chartData, xAxisStartTick, xAxisLabel) {
   });
 }
 
-function drawBarChart(domId, data, xAxisStartTick, xAxisLabel) {
+function drawCountryBarChart(domId, data) {
+  console.log(data);
+  let xAxis = ['x'];
+  let males = ['Male'];
+  let females = ['Female'];
+  let total = ['Total'];
+  data.forEach(item => {
+    xAxis.push(item.country);
+    males.push(item.males);
+    females.push(item.females);
+    total.push(item.total);
+  });
+  chartData = [xAxis, males, females, total];
+  console.log(chartData);
   var chart = c3.generate({
     bindto: domId,
     size: {
         height: 400
     },
     data: {
-      columns: data,
+      x: 'x',
+      columns: chartData,
       type: 'bar'
-    },
-    bar: {
-        width: {
-            ratio: 10 // this makes bar width 50% of length between ticks
-        }
-        // or
-        //width: 100 // this makes bar width 100px
     },
     axis: {
       x: {
+        type: 'category',
         tick: {
-          format: function (x) { return x + xAxisStartTick; }
-        }
-      },
-      y: {
-        label: { // ADD
-          text: 'Population',
-          position: 'outer-middle'
-        }
-      },
-      rotated: true
-    },
-    point: {
-        show: false
+          rotate: 75,
+          multiline: false
+        },
+        height: 130
+      }
     },
     zoom: {
-        enabled: true
-    },
-    tooltip: {
-        grouped: true // Default true
+      enabled: true
     }
   });
+  // var chart = c3.generate({
+  //   bindto: domId,
+  //   size: {
+  //       height: 400
+  //   },
+  //   data: {
+  //     columns: data,
+  //     type: 'bar'
+  //   },
+  //   bar: {
+  //       width: {
+  //           ratio: 10 // this makes bar width 50% of length between ticks
+  //       }
+  //       // or
+  //       //width: 100 // this makes bar width 100px
+  //   },
+  //   axis: {
+  //     x: {
+  //       tick: {
+  //         format: function (x) { return x + xAxisStartTick; }
+  //       }
+  //     },
+  //     y: {
+  //       label: { // ADD
+  //         text: 'Population',
+  //         position: 'outer-middle'
+  //       }
+  //     },
+  //     rotated: true
+  //   },
+  //   point: {
+  //       show: false
+  //   },
+  //   zoom: {
+  //       enabled: true
+  //   },
+  //   tooltip: {
+  //       grouped: true // Default true
+  //   }
+  // });
 }
 
-function drawLifeExpectancyBarChart(domId, data, xAxisStartTick, xAxisLabel) {
+function drawLifeExpectancyBarChart(domId, data) {
+  // put the data in to the correct data format
+  let xAxis = ['x'];
+  let lifeExpectancy = ['Life Expectancy'];
+  data.forEach(item => {
+    xAxis.push(item.country);
+    lifeExpectancy.push(item.total_life_expectancy);
+  });
+  // console.log(xAxis, xAxis.length);
+  // console.log(lifeExpectancy, lifeExpectancy.length);
   var chart = c3.generate({
     bindto: domId,
     size: {
         height: 400
     },
     data: {
-      columns: data,
+      x: 'x',
+      columns: [
+        xAxis,
+        lifeExpectancy,
+      ],
       type: 'bar'
-    },
-    bar: {
-        width: {
-            ratio: 15 // this makes bar width 50% of length between ticks
-        }
-        // or
-        //width: 100 // this makes bar width 100px
     },
     axis: {
       x: {
-        label: {
-          text: xAxisLabel,
-          position: 'outer-middle'
-        },
+        type: 'category',
         tick: {
-          format: function (x) { return x + xAxisStartTick; }
-        }
-      },
-      y: {
-        label: { // ADD
-          text: 'Years',
-          position: 'outer-middle'
-        }
-      },
-      rotated: false
-    },
-    point: {
-        show: false
+          rotate: 75,
+          multiline: false
+        },
+        height: 130
+      }
     },
     zoom: {
-        enabled: true
-    },
-    tooltip: {
-        grouped: true // Default true
+      enabled: true
     }
   });
 }
@@ -524,7 +556,20 @@ $("#country-chart-form").on("submit", function() {
   let age = $('#country-chart-age').val();
   let year = $('#country-chart-year').val();
   getPopulationByAgeGroupAndYear(age, year)
-    .then(populationData => drawBarChart('#country-chart', populationData, 0, 'Country'))
+    .then(populationData => drawCountryBarChart('#country-chart', populationData, 0, 'Country'))
+    .catch(catchErr);
+  return false;
+});
+
+$("#life-expectancy-chart-form").on("submit", function() {
+  let sex = $('input[name=sex]:checked').val();
+  let month = $('#life-expectancy-chart-month').val();
+  let day = $('#life-expectancy-chart-day').val();
+  let year = $('#life-expectancy-chart-year').val();
+  let country = $('#life-expectancy-chart-country').val();
+  let dob = year + '-' + month + '-' + day;
+  getTotalLifeExpectancyAllCountries(sex, country, dob)
+    .then(totalLifeExpectancy => drawLifeExpectancyBarChart('#life-expectancy-chart', totalLifeExpectancy))
     .catch(catchErr);
   return false;
 });
